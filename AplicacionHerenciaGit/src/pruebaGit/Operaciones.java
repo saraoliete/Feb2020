0d8988f5e2d55752ac734d7450994ec63d969517
Sara <saraoliete9@gmail.com>
2020-02-07 18:50:04 +0100

    Agregada la clase Operaciones

---
+++ AplicacionHerenciaGit/src/pruebaGit/Operaciones.java
@@ -0,0 +1,52 @@
+package pruebaGit;
+
+public class Operaciones {
+	
+	
+	int operadorA, operadorB;
+	
+	
+	public Operaciones(int operadorA, int operadorB) {
+		
+		
+		this.operadorA=operadorA;
+		this.operadorB=operadorB;
+		
+		
+	}
+
+
+	/**
+	 * @return the operadorA
+	 */
+	public int getOperadorA() {
+		return operadorA;
+	}
+
+
+	/**
+	 * @param operadorA the operadorA to set
+	 */
+	public void setOperadorA(int operadorA) {
+		this.operadorA = operadorA;
+	}
+
+
+	/**
+	 * @return the operadorB
+	 */
+	public int getOperadorB() {
+		return operadorB;
+	}
+
+
+	/**
+	 * @param operadorB the operadorB to set
+	 */
+	public void setOperadorB(int operadorB) {
+		this.operadorB = operadorB;
+	}
+	
+	
+
+}
